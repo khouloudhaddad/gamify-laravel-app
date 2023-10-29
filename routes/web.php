8f767b3dd4260b4ca00bc3c2f11d000e80853282
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-10-29 10:35:09 +0000

    Test functions

--- routes/web.php
+++ routes/web.php
@@ -21,22 +21,20 @@ Route::get('/', function () {
     //$addLevel = Level::add(['level' => 1, 'next_level_experience' => null], ['level' => 2, 'next_level_experience' => 100], ['level' => 3, 'next_level_experience' => 250]);
     //return view('welcome');
     $user = User::find(1);
-    $giveExp = $user->addPoints(1);
-    dump($giveExp);
-    // $setExp = $user->setPoints(90);
-    // $getPoints = $user->getPoints();
-    // $getLevel = $user->getLevel();
-    // $getNextLevel = $user->nextLevelAt();
-    // dump($getPoints);
-    // dump($getLevel);
-    // dump($getNextLevel);
+    $setExp = $user->setPoints(90);
+    $getPoints = $user->getPoints();
+    $getLevel = $user->getLevel();
+    $getNextLevel = $user->nextLevelAt();
+    dump($getPoints);
+    dump($getLevel);
+    dump($getNextLevel);
 
-    // $giveExp = $user->addPoints(10);
-    // $getPoints = $user->getPoints();
-    // $getLevel = $user->getLevel();
-    // $getNextLevel = $user->nextLevelAt();
+    $giveExp = $user->addPoints(10);
+    $getPoints = $user->getPoints();
+    $getLevel = $user->getLevel();
+    $getNextLevel = $user->nextLevelAt();
 
-    // dump($getPoints);
-    // dump($getLevel);
-    // dump($getNextLevel);
+    dump($getPoints);
+    dump($getLevel);
+    dump($getNextLevel);
 });
